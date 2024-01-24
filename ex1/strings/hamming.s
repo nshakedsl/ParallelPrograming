41ab9075918f0c1978eb26eaddcba13a54aa5917
OfirGurvits <ofirgurvits@gmail.com>
2024-01-24 16:29:39 +0200

    better compare

--- ex1/strings/hamming.s
+++ ex1/strings/hamming.s
@@ -1,7 +1,6 @@
 # 325655058 ofir gurvits
 # 323082867 Shaked Solomon
 .globl hamming_dist
-.globl b64_distance
 .section .data
     .align 16
 .section .rodata
@@ -18,11 +17,12 @@ hamming_dist: #recieves as arguments two pointers %rdi = &str1 %rsi = &str2
     addq    %rcx,%rax   # add the amount of matching chars to the return value
     movdqa  (%rdi), %xmm0 # load the first 16 chars
     movdqa  (%rsi), %xmm1 # load the first 16 chars
-    subq    $16, %rdi   # move forward 16 characters
-    subq    $16 ,%rsi   # move forward 16 characters
-    pcmpistri $0x0, %xmm0, %xmm1  # compare str1 and str2
-    je .L4              #if null byte was encountered in xmm2 then zf is 0
-    jns .L4             #if null byte was encountered in xmm1 then zf is 0
+    addq    $16, %rdi   # move forward 16 characters
+    addq    $16 ,%rsi   # move forward 16 characters
+    pcmpistri $0x10, %xmm0, %xmm1  # compare str1 and str2
+    jne .L4             # if null byte was encountered in xmm2 then zf is 0
+    jns .L4             # if null byte was encountered in xmm1 then zf is 0
+    addq    %rcx,%rax   # add the amount of matching chars to the return value
 
 	movq	%rbp, %rsp	#restore stack
 	popq	%rbp
