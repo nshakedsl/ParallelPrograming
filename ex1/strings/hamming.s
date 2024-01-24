2fe56260b889f07c19513bc0db54b81d03ae6a86
OfirGurvits <ofirgurvits@gmail.com>
2024-01-25 01:57:11 +0200

    kinda working?

--- ex1/strings/hamming.s
+++ ex1/strings/hamming.s
@@ -1,5 +1,7 @@
 # 325655058 ofir gurvits
 # 323082867 Shaked Solomon
+.intel_syntax noprefix
+
 .globl hamming_dist
 .section .data
     .align 16
@@ -7,23 +9,31 @@
 
 .text	#the beginnig of the code
 hamming_dist: #recieves as arguments two pointers %rdi = &str1 %rsi = &str2
-    pushq	%rbp		#save the old frame pointer
-    movq	%rsp, %rbp
-    pushq   %rbx        #push rbx to stack for later
+    push	rbp		#save the old frame pointer
+    mov	    rbp,rsp
+    push    rbx        #push rbx to stack for later
 
-    xorq    %rax,%rax
-    xorq    %rcx,%rcx
+    xor    rax,rax
+    xor    rcx,rcx
+    pxor   xmm0,xmm0
     .L4:
-    addq    %rcx,%rax   # add the amount of matching chars to the return value
-    movdqa  (%rdi), %xmm0 # load the first 16 chars
-    movdqa  (%rsi), %xmm1 # load the first 16 chars
-    addq    $16, %rdi   # move forward 16 characters
-    addq    $16 ,%rsi   # move forward 16 characters
-    pcmpistri $0x10, %xmm0, %xmm1  # compare str1 and str2
+    pmovmskb ecx,xmm0 # load the msb of each byte to a bit in eax
+    popcnt  ecx,ecx   # count matching chars
+    addq    rcx,rax   # add the amount of matching chars to the return value
+    movdqa  xmm1, [rdi] # load the first 16 chars
+    movdqa  xmm2, [rsi] # load the first 16 chars
+    add    rdi,16   # move forward 16 characters
+    add    rsi,16   # move forward 16 characters 00010010
+    mov    rbx,0b00100010
+    pcmpistrm xmm1, xmm2, 0b01001000 # Compare for equality, result in XMM0
     jne .L4             # if null byte was encountered in xmm2 then zf is 0
     jns .L4             # if null byte was encountered in xmm1 then zf is 0
-    addq    %rcx,%rax   # add the amount of matching chars to the return value
 
-	movq	%rbp, %rsp	#restore stack
-	popq	%rbp
+    pmovmskb ecx,xmm0 # load the msb of each byte to a bit in eax
+    popcnt   ecx,ecx   # count matching chars
+    add      rax,16   # add the amount of matching chars to the return value
+    sub      rax,rcx
+
+	mov 	 rsp, rbp	#restore stack
+	pop 	 rbp
 	ret			        #return turn
