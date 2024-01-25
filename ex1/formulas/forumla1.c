57ad662b5f40a2acd4f19ec7fb22fb0cb5489a09
OfirGurvits <ofirgurvits@gmail.com>
2024-01-25 18:58:42 +0200

    AVX

--- ex1/formulas/forumla1.c
+++ ex1/formulas/forumla1.c
@@ -1,18 +1,29 @@
 #define SIZE_TOT 256
 #define NUM_OF_FLOATS SIZE_TOT/(sizeof(float)*8)
 #include "immintrin.h"
+
 float formula1(float *x, unsigned int length){
     float sum = 0;
+    float bot_mul, placeholder[8];
     __m256 loaded; // var to load from memory the vectors
     __m256 bottom_mul = _mm256_set1_ps(1.0);
     __m256 ONE_ARRAY = _mm256_set1_ps(1.0);
     __m256 top_addition = _mm256_setzero_ps();
-    for (int i = 0; i < length; i+=NUM_OF_FLOATS) {
+    int i;
+    for (i = 0; i < length; i+=NUM_OF_FLOATS) {
         //load current 8 floats
         loaded = _mm256_load_ps(x);
         bottom_mul = _mm256_mul_ps(bottom_mul, _mm256_fmadd_ps(loaded,loaded,ONE_ARRAY)); //calculate Pi Xk^2 +1
-
+        top_addition = _mm256_add_ps(top_addition, _mm256_sqrt_ps(loaded)); // calculate sigma sqrt(Xk)
     }
+    //store the resulting values of the bottom to bot_mul
+    _mm256_storeu_ps(placeholder,bottom_mul);
+    bot_mul = placeholder[0] * placeholder[1] *placeholder[2] * placeholder[4] * placeholder[5] * placeholder[6] *
+            placeholder[7] * placeholder[3];
 
-    return 0;
+    //sum all leftover elements
+    for(; i< length; ++i){
+        bot_mul *= x[i]*x[i] + 1;
+    }
+    return sum;
 }
